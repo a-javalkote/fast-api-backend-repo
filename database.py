208f461aa318d62436a8d2621d8fcbe0116be0d6
Bhimashankar Prakash Javalkote <72732496+MrAkashCoder@users.noreply.github.com>
2021-04-21 21:51:10 +0530

    Updaetd

--- database.py
+++ database.py
@@ -3,9 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL ="mysql+mysqlconnector://root:@localhost:3306/apitheprimepr"
-#SQLALCHEMY_DATABASE_URL ="sqlite:///theprimepr.db"
-#DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL ="mysql+mysqlconnector://treadte1_treadte1:treadte1_treadte1@treadtechnology.com:3306/treadte1_apitheprimepr"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
