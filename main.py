f1d119e3a86b18d1efff1b9e165bf4ff96290597
Bhimashankar Prakash Javalkote <72732496+MrAkashCoder@users.noreply.github.com>
2021-04-24 12:50:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from database import Base, engine, get_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:4200",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(home.router)
