9d60a1ccf23d4df913809980f2b339b8f8c8636e
Bhimashankar Prakash Javalkote <72732496+MrAkashCoder@users.noreply.github.com>
2021-05-08 12:58:31 +0530

    Update

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get('/',response_class=HTMLResponse)
